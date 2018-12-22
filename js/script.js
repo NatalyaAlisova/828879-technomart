db2a692993144767e5f09ea2a9fbc4f8dc2ec70d
NatalyaAlisova <lara44@yandex.ru>
2018-12-22 23:49:00 +0300

    сделано js на модальное окно с картой в index.html

--- js/script.js
+++ js/script.js
@@ -1,30 +1,116 @@
 console.log('файл скриптов подключен');
 
+//---------------------------------------
+// Этот блок относится к каталогу
+//---------------------------------------
 // объявление переменных
-var addToBasket = document.querySelectorAll('.product-hidden .buy');
- 	console.log(addToBasket);
-var popup = document.querySelector('.modal-added-cart');
-var close = document.querySelector('.modal-close');
+var addToBasketBtns = document.querySelectorAll('.product-hidden .buy');
+ 	console.log(addToBasketBtns);
+var popupAddToBasket = document.querySelector('.modal-added-cart');
+var modalCloseBtn = document.querySelector('.modal-close');
+var continueShoppingBtn = document.querySelector('.continue-shopping');
+
 
 // вешаем обработчик на все кнопки купить
-for (var i = 0; i < addToBasket.length; i++) 
-  {
+for (var i = 0; i < addToBasketBtns.length; i++) {
    		console.log(i);
-		addToBasket[i].addEventListener ("click", function (evt) 
-			{
+		addToBasketBtns[i].addEventListener ("click", function (evt) {
 			    evt.preventDefault();
 				console.log('нажали на кнопку');
-				popup.classList.add('modal-show');
-			}
-		); 
-
-  }
+				popupAddToBasket.classList.add('modal-show');
+		}); 
+	}
 
 //  закрывем модалку
-close.addEventListener ("click", function (evt) 
-	{
+if (modalCloseBtn) {
+	modalCloseBtn.addEventListener ("click", function (evt) {
 	    evt.preventDefault();
 		console.log('нажали закрыть');
-		popup.classList.remove('modal-show');
-	}
-);
+		if (popupAddToBasket) { 
+			popupAddToBasket.classList.remove('modal-show');
+		}
+	});
+}
+// закрываем модалку по кнопке "Продолжить покупки"
+
+
+if (continueShoppingBtn) {
+	continueShoppingBtn.addEventListener ("click", function (evt) {
+	    evt.preventDefault();
+		console.log('нажали на кнопку "Продолжить покупки');
+		popupAddToBasket.classList.remove('modal-show');
+	});
+}
+
+// закрываем модалки ESC
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popupAddToBasket.classList.contains("modal-show")) {
+        popupAddToBasket.classList.remove("modal-show");
+       }
+    }
+  });
+
+
+//---------------------------------------
+// Показываем карту
+//---------------------------------------
+
+
+var mapContact = document.querySelector('.contact img');
+
+var mapModal = document.querySelector('.modal-map');
+var mapClose = mapModal.querySelector('.modal-close');
+
+// открываем карту 
+if (mapContact) {
+mapContact.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   console.log('нажали посмотреть карту');
+   mapModal.classList.add("modal-show");
+  });
+}
+
+//  закрывем модалку
+if (mapClose) {
+mapClose.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   console.log('нажали закрыть карту');
+   mapModal.classList.remove("modal-show");
+  });
+}
+
+// закрываем модалки ESC
+window.addEventListener("keydown", function (evt) {
+   evt.preventDefault();
+   if (evt.keyCode === 27) {
+     if (mapModal.classList.contains("modal-show")) {
+       mapModal.classList.remove("modal-show");
+      }
+   }
+  });
+
+//---------------------------------------
+// Показываем окно с обратной связью
+//---------------------------------------
+
+var buttonWrite = document.querySelector('.button-write');
+
+var modalSearch = document.querySelector('.modal-search');
+var modalClose = modalSearch.querySelector('.modal-close');
+
+var modalName = modalSearch.querySelector("[name=name]");
+
+// открываем окно обратной связи
+buttonWrite.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modalSearch.classList.add("modal-show");
+    name.focus();
+  });
+
+// закрываем модалку
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
